250c5a3765f3f29ae2fe567099bd8fe4cca8cdd3
your name <shafiarnob@gmail.com>
2024-06-09 07:13:38 +0600

    render menu items on display

--- password-manager/password-manager.ino
+++ password-manager/password-manager.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <WS2812FX.h>
 #include <U8g2lib.h>
-
+#include <ArduinoJson.h>
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
@@ -12,9 +12,11 @@
 #define LED_PIN 12
 
 // WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
-
+DynamicJsonDocument doc(1000); 
 
 U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // EastRising 0.42" OLED
+
+String password = "[{\"id\":1,\"name\":\"Gmail\"},{\"id\":2,\"name\":\"Facebook\"},{\"id\":3,\"name\":\"Github\"},{\"id\":4,\"name\":\"Instagram\"}]";
 
 void u8g2_prepare(void) {
   u8g2.setFont(u8g2_font_6x10_tf);
@@ -35,8 +37,16 @@ void setup() {
 void loop() {
   u8g2.clearBuffer();
   u8g2_prepare();
-  u8g2.drawStr( 0, 0, "ASCII page--");
-  u8g2.drawStr( 0, 8, "ASCII page--");
+
+  int render_menu = 0;
+
+  deserializeJson(doc, password);
+  for (JsonObject elem : doc.as<JsonArray>()) {
+    const char* menu_item_name = elem["name"];
+    u8g2.drawStr( 0, render_menu, menu_item_name);
+    render_menu+=8;
+  }
+
   u8g2.sendBuffer();
   delay(100);
 }
